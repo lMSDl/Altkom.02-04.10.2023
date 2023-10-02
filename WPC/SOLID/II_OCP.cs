a1c3f75e97df0ff3bac1000d29c7e646b4f5c431
Paweł Łęczycki <pawel@msd.info.pl>
2023-10-02 12:15:31 +0200

    OCP

---
+++ WPC/SOLID/II_OCP.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WPC.SOLID.O
+{
+    abstract class Shape
+    {
+        public abstract double Area {get;}
+        //public abstract double GetArea();
+    }
+
+    class Square : Shape
+    {
+        public int A { get; set; }
+
+        public override double Area => A * A;
+
+        /*public override double GetArea()
+        {
+            return A * A;
+        }*/
+    }
+
+    class Rectangle : Shape
+    {
+        public int A { get; set; }
+        public int B { get; set; }
+        public override double Area => A * B;
+
+        /*public override double GetArea()
+        {
+            return A * B;
+        }*/
+    }
+
+    class Circle : Shape
+    {
+        public int R { get; set; }
+        public override double Area => R * R * Math.PI;
+
+        /*public override double GetArea()
+        {
+            return R * R * Math.PI;
+        }*/
+    }
+
+    class ShapeCalculator
+    {
+        double Area(Shape shape)
+        {
+            return shape.Area;
+        }
+    }
+
+            /*class ShapeCalculator
+            {
+                double Area(Shape shape)
+                {
+                    switch (shape)
+                    {
+                        case Square square:
+                            return square.A * square.A;
+                        case Rectangle rectangle:
+                            return rectangle.A * rectangle.B;
+                        case Circle circle:
+                            return Math.PI * circle.R * circle.R;
+                        default:
+                            return 0;
+                    }
+                }
+            }*/
+        }
