2f8640f0bf1b2e7d3bb9eae2e1d33905422e592e
Paweł Łęczycki <pawel@msd.info.pl>
2023-10-02 11:58:31 +0200

    SRP

---
+++ WPC/SOLID/I_SRP.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WPC.SOLID.S
+{
+
+    public class User
+    {
+        public string UserName { get; set; }
+        public string Password { get; set; }
+        public string Email { get; set; }
+    }
+
+    public static class Validators { 
+        public static bool ValidateEmail(string email)
+        {
+            try
+            {
+                var addr = new System.Net.Mail.MailAddress(email);
+                return addr.Address == email;
+            }
+            catch
+            {
+                return false;
+            }
+        }
+    }
+    /*public class User
+    {
+        public string UserName { get; set; }
+        public string Password { get; set; }
+        public string Email { get; set; }
+
+        public bool ValidateEmail()
+        {
+            try
+            {
+                var addr = new System.Net.Mail.MailAddress(Email);
+                return addr.Address == Email;
+            }
+            catch
+            {
+                return false;
+            }
+        }
+    }*/
+}
