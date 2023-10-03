38a0769ab30d1a4c659360e48ef3890b92b033b2
Paweł Łęczycki <pawel@msd.info.pl>
2023-10-03 13:21:48 +0200

    Flyweight: przed

---
+++ WPC/DesignPatterns/Structural/Flyweight/Car.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WPC.DesignPatterns.Structural.Flyweight
+{
+    public class Car
+    {
+        public string Owner { get; set; }
+        public string Number { get; set; }
+        public string Manufacturer { get; set; }
+        public string Model { get; set; }
+        public string Color { get; set; }
+    }
+}
---
+++ WPC/DesignPatterns/Structural/Flyweight/Client.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WPC.DesignPatterns.Structural.Flyweight
+{
+    class Client
+    {
+        public static void Execute()
+        {
+            var car1 = new Car
+            {
+                Owner = "John Doe",
+                Number = "AMS8786",
+                Manufacturer = "Opel",
+                Model = "Astra I",
+                Color = "Pink"
+            };
+
+            var car2 = new Car
+            {
+                Owner = "Ewa Ewowska",
+                Number = "AAMS886",
+                Manufacturer = "Opel",
+                Model = "Astra I",
+                Color = "Black"
+            };
+
+            var car3 = new Car
+            {
+                Owner = "Ewa Ewowska",
+                Number = "ASK99210",
+                Manufacturer = "Opel",
+                Model = "Astra I",
+                Color = "Black"
+            };
+        }
+    }
+}
