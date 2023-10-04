1c0954b8f28f09c506a95fd785c7f9eb9cfbeceb
Paweł Łęczycki <pawel@msd.info.pl>
2023-10-04 09:11:28 +0200

    Command: przed

---
+++ WPC/DesignPatterns/Behavioral/Command/Garden.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WPC.DesignPatterns.Behavioral.Command
+{
+    class Garden
+    {
+        public int Size { get; }
+        private List<string> Plants { get; } = new List<string>();
+
+        public Garden(int size)
+        {
+            Size = size;
+        }
+
+        public bool Add(string name)
+        {
+            if (Plants.Count < Size)
+            {
+                Plants.Add(name);
+                Console.WriteLine($"Roślina {name} zasadzona w ogrodzie");
+                return true;
+            }
+            Console.WriteLine($"Brak miejsca na {name}");
+            return false;
+        }
+
+        public bool Remove(string name)
+        {
+            var result = Plants.Remove(name);
+            if (result)
+                Console.WriteLine($"Roślina {name} usunięta z ogrodu");
+            else
+                Console.WriteLine($"W ogrodznie nie ma {name}");
+            return result;
+        }
+
+        public override string ToString()
+        {
+            var builder = new StringBuilder();
+            builder.AppendLine($"W ogrodznie jest {Plants.Count} roślin:");
+            foreach (var name in Plants)
+            {
+                builder.AppendLine(name);
+            }
+            return builder.ToString();
+        }
+    }
+}
